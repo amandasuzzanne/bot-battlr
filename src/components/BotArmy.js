cffca86344ce2d41ec7ca4b2d264e95145e4ecef
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-05 21:21:49 +0300

    Update BotArmy file

--- src/components/BotArmy.js
+++ src/components/BotArmy.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import "./BotArmy.css"
 
-function BotArmy() {
+function BotArmy({enlistedBots, releaseBot}) {
   return (
     <div>
-
+        <h2>Bot Army</h2>
+        <div className="bot-army">
+        {enlistedBots.map(bot => (
+            <div key={bot.id}>
+                <p>{bot.name}</p>
+                <button onClick={() => releaseBot(bot.id)}>Release Bot</button>
+            </div>
+        ))}
+        </div>
     </div>
   )
 }
 
-export default BotArmy
+export default BotArmy;
