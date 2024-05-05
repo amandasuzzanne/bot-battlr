13288f59bb37dabd394e5e88973f09925cb2200c
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-05 21:22:04 +0300

    Update BotCard file

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import "./BotCard.css"
 
-function BotCard() {
+function BotCard({bot, action, buttonText}) {
   return (
-    <div>
-
+    <div className="bot-card">
+        <img src={bot.avatar_url} alt={bot.name} />
+        <div className="bot-details">
+            <h3>{bot.name}</h3>
+            <p>Health: {bot.health}</p>
+            <p>Damage: {bot.damage}</p>
+            <p>Armor: {bot.armor}</p>
+            <p>Class: {bot.bot_class}</p>
+            <button onClick={action}>{buttonText}</button>
+        </div>
     </div>
-  )
+  );
 }
 
-export default BotCard
+export default BotCard;
