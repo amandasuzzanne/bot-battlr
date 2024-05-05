d009135faf6ac1419b7a72aa58195e4140e9e9ce
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-06 00:00:51 +0300

    Update file

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,20 +1,62 @@
 import React from 'react'
 import "./BotCard.css"
 
-function BotCard({bot, action, buttonText}) {
-  return (
-    <div className="bot-card">
-        <img src={bot.avatar_url} alt={bot.name} />
-        <div className="bot-details">
-            <h3>{bot.name}</h3>
-            <p>Health: {bot.health}</p>
-            <p>Damage: {bot.damage}</p>
-            <p>Armor: {bot.armor}</p>
-            <p>Class: {bot.bot_class}</p>
-            <button onClick={action}>{buttonText}</button>
+const botClasses = {
+    Assault: "icon military",
+    Defender: "icon shield",
+    Support: "icon plus circle",
+    Medic: "icon ambulance",
+    Witch: "icon magic",
+    Captain: "icon star",
+};
+
+function BotCard({ bot, clickEvent, deleteBot }) {
+    return (
+
+        <div className='ui column'>
+            <div className='bot-card' key={bot.id} onClick={() => clickEvent(bot)}>
+                <div className='bot-image'>
+                    <img alt='an image of a bot' src={bot.avatar_url} />
+                </div>
+
+                <div className='main-content'>
+                    <div>
+                        {bot.name}
+                        <i className={botClasses[bot.bot_class]} />
+                    </div>
+                    <div>
+                        <small>{bot.catchphrase}</small>
+                    </div>
+                </div>
+
+                <div className='other-content'>
+                    <span>
+                        <i className="icon heartbeat" />
+                        {bot.health}
+                    </span>
+                    <span>
+                        <i className="icon lightning" />
+                        {bot.damage}
+                    </span>
+                    <span>
+                        <i className="icon shield" />
+                        {bot.armor}
+                    </span>
+                    <span>
+                        <div className="ui center aligned segment basic">
+                            <button className="ui mini red button"
+                                onClick={(event) => {
+                                    event.stopPropagation();
+                                    deleteBot(bot);
+                                }}
+                            > X
+                            </button>
+                        </div>
+                    </span>
+                </div>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default BotCard;
