18ebf2b84b84561ec47ad561a2d54446a52cf727
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-05 21:23:55 +0300

    Update BotCollection file

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -3,18 +3,18 @@ import "./BotCollection.css"
 import BotCard from './BotCard'
 
 // Function that will be responsible for listing all available bots
-function BotCollection() {
+function BotCollection({bots, enlistBot, selectBot}) {
   return (
     <div>
         <h2>Bot Collection</h2>
         <div className="bot-collection">
             {/* Mapping through the bots array passed as a property and rendering a BotCard for each bot */}
-            {BotDetails.map(bot => (
-                <BotCard key={bot.id} bot={bot} enlistBot={enlistBot}/>
-            ))}
+            {bots.map(bot => (
+            <BotCard key={bot.id} bot={bot} enlistBot={enlistBot} selectBot={selectBot} />
+        ))}
         </div>
     </div>
   )
 }
 
-export default BotCollection
+export default BotCollection;
