27fd45784850eb2d6e8f004472afd6752cfe42d1
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-06 00:00:37 +0300

    Update file

--- src/components/BotArmy.js
+++ src/components/BotArmy.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import "./BotArmy.css"
+import BotCard from './BotCard';
 
-function BotArmy({enlistedBots, releaseBot}) {
-  return (
-    <div>
-        <h2>Bot Army</h2>
-        <div className="bot-army">
-        {enlistedBots.map(bot => (
-            <div key={bot.id}>
-                <p>{bot.name}</p>
-                <button onClick={() => releaseBot(bot.id)}>Release Bot</button>
+function BotArmy({bots, removeBot, deleteBot}) {
+    const botArmyItem = bots.map((bot) => {
+        return (
+          <BotCard
+            key={bot.id}
+            bot={bot}
+            clickEvent={removeBot}
+            deleteBot={deleteBot}
+          />
+        );
+      });
+    
+      return (
+        <div className="ui segment inverted olive bot-army">
+          <div className="ui five column grid">
+            <div className="row bot-army-row">
+              {botArmyItem}
             </div>
-        ))}
+          </div>
         </div>
-    </div>
-  )
-}
+      );
+    }
 
 export default BotArmy;
