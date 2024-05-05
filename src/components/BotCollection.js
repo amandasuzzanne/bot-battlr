0f874e7e71bd38a0709374116f5c1d0444f8b92c
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-05 15:35:13 +0300

    Create function for listing all bots

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./BotCollection.css"
+import BotCard from './BotCard'
 
+// Function that will be responsible for listing all available bots
 function BotCollection() {
   return (
     <div>
-
+        <h2>Bot Collection</h2>
+        <div className="bot-collection">
+            {/* Mapping through the bots array passed as a property and rendering a BotCard for each bot */}
+            {BotDetails.map(bot => (
+                <BotCard key={bot.id} bot={bot} enlistBot={enlistBot}/>
+            ))}
+        </div>
     </div>
   )
 }
